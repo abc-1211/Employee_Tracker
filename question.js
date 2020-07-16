b99171d465171cfed693e567828b9055425ca5e7
ian <chengkwanyu@yahoo.com.hk>
2020-07-16 14:26:43 +0800

    question implemented

--- question.js
+++ question.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const Employee = require("./classconstructor");
+
 
 const path = [
     {
@@ -41,11 +43,35 @@ const path = [
     }
 ];
 
+const addEmployee = [
+    {
+        message: "Enter Employee First Name:",
+        name: "first_name"
+    },
+    {
+        message: "Enter Employee Last Name:",
+        name: "last_name"
+    },
+    {
+        message: "Enter Role ID:",
+        name: "role_id"
+    },
+    {
+        message: "Enter Manager ID (Press ENTER if no manager):",
+        name: "manager_id"
+    }
+];
 
+// Testing Area
 async function ans() {
-    var ans = await inquirer.prompt(path);
-    console.log(ans);
-    console.log(ans.action);
+    var { first_name, last_name, role_id, manager_id } = await inquirer.prompt(addEmployee);
+    //console.log(ans);
+    console.log(first_name);
+    
+    var newppl = new Employee( first_name, last_name, role_id, manager_id );
+    console.log("\n" + JSON.stringify(newppl));
 }
 
 ans()
+
+module.exports = { path, addEmployee};
